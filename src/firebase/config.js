e5bd95e70efc5b471fb48d4acafccaa48fae1479
PratikSutar97 <pratiksutar1901@gmail.com>
2026-01-08 15:49:16 +0530

    config updated

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,12 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsc-rBsXlgeXSjXPfXRWrYIZaRSFoCCRk",
-  authDomain: "infosphereee-2dedb.firebaseapp.com",
-  projectId: "infosphereee-2dedb",
-  storageBucket: "infosphereee-2dedb.firebasestorage.app",
-  messagingSenderId: "754015635046",
-  appId: "1:754015635046:web:3db205ca17fd5e6afd0fa7",
-  measurementId: "G-FVVQ18BFYW"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
